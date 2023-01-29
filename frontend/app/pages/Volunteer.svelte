42ce3874dbce7ed0f74ab6a18fb5158cdc794e56
Matthew chen <Matgenius04@users.noreply.github.com>
2023-01-29 10:52:58 -0600

    trying out random combos of certs
    because I'm desperate

---
+++ frontend/app/pages/Volunteer.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import camera from 'nativescript-camera'
+</script>
+<page>
+  <actionBar>
+    <label text="Shovelmates" fontSize="25"></label>
+  </actionBar>
+  <flexboxLayout>
+    <stackLayout>
+      <label text="Picture submission"></label>
+      <button text="Submit Picture"></button>
+    </stackLayout>
+    <stackLayout>
+
+    </stackLayout>
+  </flexboxLayout>
+</page>
